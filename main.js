d82a7d5605ab11b2fa94f74945dc39b29f1e2567
Alisherhoja <hakeralisher1999@gmail.com>
2025-02-21 23:59:39 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+// const cars = ["BMW" , "AUDI" , "MAZDA" , "LEXUS"];
+
+// const a=[];
+
+// cars.forEach(moshinalar => {
+//     a.push(moshinalar);
+
+
+
+// })
+// console.log(a);
+
+
+
+
+
+
+// const numbers = [3,7,1,3,3,1];
+
+
+
+// let result = 3 + 7 + 1 + 3 + 3 + 1;
+
+
+// console.log(result);
+
+
+
+
+
+
+
+const cars = ["non" , "banan" , "mashina" , `n harfi ishtirok etgan sozlar` ];
+
+const carss= ["asal" , "olma" , "aziza" , "gul" , "n harfi ishtirok etmagan sozlar"  ];
+
+const a =[];
+
+cars.forEach(moshinalar => {
+
+
+
+    a.push(moshinalar)
+})
+console.log(a);
+
+
+
+
+
+
+const b =[];
+
+carss.forEach(moshinalar => {
+
+
+
+    b.push(moshinalar)
+})
+console.log(b);
